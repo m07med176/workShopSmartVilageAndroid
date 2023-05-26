87f47c372f24fd8e63848c982f9f57cc6aca4fc7
Hussien Ahmed <45578079+HusseinAhmedAbdelhammed@users.noreply.github.com>
2023-05-26 12:33:54 +0300

    addedValidation

--- app/src/main/java/iti/workshop/newApp/registration/RegistrationFragment.kt
+++ app/src/main/java/iti/workshop/newApp/registration/RegistrationFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import iti.workshop.newApp.R
+import iti.workshop.newApp.helpers.ValidationReg
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,18 +21,35 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RegistrationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+lateinit var userName:EditText
+lateinit var password:EditText
+lateinit var confPassword:EditText
+lateinit var email:EditText
+lateinit var submit:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init(view)
+        submit.setOnClickListener(View.OnClickListener {
+            if (ValidationReg.validateData(password.text.toString(),email.text.toString(),confPassword.text.toString())){
+
+            }else{
+                
+            }
+        })
+    }
+fun init(view:View){
+    userName=view.findViewById(R.id.userName)
+    password=view.findViewById(R.id.passWord)
+    email=view.findViewById(R.id.email)
+    submit=view.findViewById(R.id.submit)
+    confPassword=view.findViewById(R.id.confPassword)
+}
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -38,23 +58,4 @@ class RegistrationFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_registration, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RegistrationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            RegistrationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
