da51ad0c4849e9cc1ad8146f2286840d5637194f
Mahmoud Sayed <62342493+MahmoudSayedAwad@users.noreply.github.com>
2023-05-26 12:48:16 +0300

    addRegisterNetworkCall

--- data/src/main/java/iti/workshop/data/source/Constants.kt
+++ data/src/main/java/iti/workshop/data/source/Constants.kt
@@ -2,8 +2,8 @@ package iti.workshop.data.source
 
 object Constants {
     const val BASE_URL = "put url here"
-    const val API_KEY = "api key here"
-
+    const val REGISTER_API_URL = "https://identitytoolkit.googleapis.com/v1/accounts:signUp"
+    const val REGISTER_API_KEY = "AIzaSyAVRNpZKTubdM9mp3L9HaE8XGj09eNoV1o"
     // Network and Room cash
     const val MAX_AGE = 7
     const val  MAX_AGE_MILLI = MAX_AGE * 24 * 60 * 60 * 1000
--- data/src/main/java/iti/workshop/data/source/remote/retrofit/CallApi.kt
+++ data/src/main/java/iti/workshop/data/source/remote/retrofit/CallApi.kt
@@ -1,4 +1,14 @@
 package iti.workshop.data.source.remote.retrofit
 
+import iti.workshop.data.source.Constants
+import iti.workshop.data.source.remote.models.RegisterationResponse
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
 interface CallApi {
+    @POST(Constants.REGISTER_API_URL + "?" + Constants.REGISTER_API_KEY)
+    suspend fun register(
+        //  @Query("key") key:String
+    ): RegisterationResponse
 }
