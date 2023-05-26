8c36f722a6e559dfeb15651fe908ced975a2a288
Mohamed Arfa <dev.mohamed.arfa@gmail.com>
2023-05-26 11:30:27 +0300

    repo handle2

--- data/src/main/java/iti/workshop/data/repository/IRepository.kt
+++ data/src/main/java/iti/workshop/data/repository/IRepository.kt
@@ -4,6 +4,13 @@ import android.content.Context
 import iti.workshop.data.source.local.ILocalDataSource
 import iti.workshop.data.source.remote.IRemoteDataSource
 
-interface IRepository: ILocalDataSource, IRemoteDataSource {
+interface IRepository{
+    fun login(username:String, password:String)
+
+    fun signUp()
+
+    fun logout()
+
+
 
 }
--- data/src/main/java/iti/workshop/data/repository/Repository.kt
+++ data/src/main/java/iti/workshop/data/repository/Repository.kt
@@ -9,5 +9,17 @@ class Repository(
     val local: ILocalDataSource,
     val remote: IRemoteDataSource
 ) : IRepository {
+    override fun login(username: String, password: String) {
+        TODO("Not yet implemented")
+    }
+
+
+    override fun signUp() {
+        TODO("Not yet implemented")
+    }
+
+    override fun logout() {
+        TODO("Not yet implemented")
+    }
 
 }
