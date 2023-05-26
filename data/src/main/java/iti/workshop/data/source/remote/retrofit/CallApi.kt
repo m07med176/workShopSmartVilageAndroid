cdd85dab9b8218d126adc20fd55c603babf31045
Mahmoud Sayed <62342493+MahmoudSayedAwad@users.noreply.github.com>
2023-05-26 14:12:52 +0300

    update register method

--- data/src/main/java/iti/workshop/data/source/remote/retrofit/CallApi.kt
+++ data/src/main/java/iti/workshop/data/source/remote/retrofit/CallApi.kt
@@ -1,14 +1,15 @@
 package iti.workshop.data.source.remote.retrofit
 
 import iti.workshop.data.source.Constants
+import iti.workshop.data.source.remote.models.RegBody
 import iti.workshop.data.source.remote.models.RegisterationResponse
-import retrofit2.http.GET
+import retrofit2.http.Body
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface CallApi {
     @POST(Constants.REGISTER_API_URL + "?" + Constants.REGISTER_API_KEY)
     suspend fun register(
-        //  @Query("key") key:String
+        @Body body: RegBody
+
     ): RegisterationResponse
 }
--- data/src/main/java/iti/workshop/data/source/remote/retrofit/RetrofitInstance.kt
+++ data/src/main/java/iti/workshop/data/source/remote/retrofit/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitInstance {
+object RetrofitInstance {
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(Constants.BASE_URL)
