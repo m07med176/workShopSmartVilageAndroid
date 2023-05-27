7ae2a5c903dc0a21e97cf5e5e75cbaeba35c2ebd
Mahmoud Sayed <62342493+MahmoudSayedAwad@users.noreply.github.com>
2023-05-27 13:28:42 +0300

    handle search Bar ui and add action bar

--- app/src/main/java/iti/workshop/newApp/MainActivity.kt
+++ app/src/main/java/iti/workshop/newApp/MainActivity.kt
@@ -1,10 +1,14 @@
 package iti.workshop.newApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+
 
 class MainActivity : AppCompatActivity() {
     lateinit var navController : NavController
@@ -22,6 +26,37 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         // Handle the navigation up button press
         return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar,menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.search -> {
+                val navHostFragment =
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+                val navController = navHostFragment.navController
+                navController.navigate(R.id.action_splashFragment_to_dashBoardFragment)
+                return true
+            }
+
+            R.id.favourite -> //add the function to perform here
+            {
+                val navHostFragment =
+                    supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+                val navController = navHostFragment.navController
+                navController.navigate(R.id.action_splashFragment_to_dashBoardFragment)
+                return true
+
+            }
+
+
+
+
+
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
 
