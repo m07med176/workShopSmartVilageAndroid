1b1f549606f3671119f0c27a6a005479ccb1d53a
AhmedAbdo12345 <Ahmedabdo12377@gmail.com>
2023-05-27 14:19:07 +0300

    create favourite ui screen, create adapter class and DiffUtil Class

--- app/src/main/java/iti/workshop/newApp/favorite/FavoriteFragment.kt
+++ app/src/main/java/iti/workshop/newApp/favorite/FavoriteFragment.kt
@@ -5,16 +5,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import iti.workshop.data.source.dto.Article
 import iti.workshop.newApp.R
+import iti.workshop.newApp.databinding.FragmentFavoriteBinding
 
-class FavoriteFragment : Fragment() {
-
+class FavoriteFragment : Fragment(),FavAdapter.ListItemClickListener,FavAdapter.ListItemClickListenerDelete {
+    lateinit var binding: FragmentFavoriteBinding
+    var favAdapter = FavAdapter(this@FavoriteFragment,this@FavoriteFragment)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(layoutInflater, container, false)
+        binding.lifecycleOwner = this
+        return binding.root    }
+
+    override fun onClickFavDelete(article: Article) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onClickFav(article: Article) {
+        TODO("Not yet implemented")
+
+       val action : FavoriteFragmentDirections.ActionFavoriteFragmentToDetailsFragment =
+        FavoriteFragmentDirections.actionFavoriteFragmentToDetailsFragment(article)
+        findNavController(requireView()).navigate(action)
+
     }
 
 }
